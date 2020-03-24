86090874a1396f3ed6edf39aa12b36e1784ea6bb
Kevin L <kevin@dynamicarsolutions.com>
2020-03-24 08:53:26 -0400

    Init Commit
    FaceTrackingUVDemo 1   - is the main scene I created.

---
+++ Assets/Scripts/TransparencyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransparencyController : MonoBehaviour
+{
+    public Renderer renderer;
+
+    [SerializeField] private Slider transparency;
+
+    [SerializeField] private Text transparencyMeter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //renderer = GetComponent<Renderer>();
+        renderer.material.shader = Shader.Find("Custom/TransparentDiffuse ZWrite");
+
+        transparency = GameObject.FindObjectOfType<Slider>();
+        transparencyMeter = FindObjectOfType<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float transparencyValue = transparency.value;
+        for (int i = 0; i < renderer.materials.Length; i++)
+        {
+            renderer.materials[i].SetVector("_Color", new Vector4(1, 1, 1, transparencyValue));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Transparency: ");
+        sb.Append(Math.Round(transparencyValue * 100, 0));
+        sb.Append("%");
+        transparencyMeter.text = sb.ToString();
+    }
+}
